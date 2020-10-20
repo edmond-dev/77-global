11b81a4bb0e573335ccf54f416ce5d2c171b7a90
Edmond Mumwensi <edmondprojects71@gamil.com>
2020-10-20 04:10:44 -0600

    new updates to some files

--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,19 @@
 const openModalButtons = document.querySelectorAll('[data-modal-target]');
-const closeModalButtons = document.querySelectorAll('[data-modal-close]');
-console.log(openModalButtons)
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+const overlay1 = document.getElementById('overlaySec');
 
 openModalButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.moda);
-    console.log(modal);
+    const modal = document.querySelector(button.dataset.modalTarget);
     openModal(modal);
   });
 });
 
 closeModalButtons.forEach(button => {
   button.addEventListener('click', () => {
-    const modal = button.closest('.modal');
+    const modal = button.closest(`.modal, .modal-content-events,
+    .modal-content-deals, .modal-content-travel, .modal-content-post`);
     closeModal(modal);
   });
 });
@@ -20,10 +21,25 @@ closeModalButtons.forEach(button => {
 
 function openModal(modal) {
   if(modal == null) return;
-  modal.classList.add('.modal');
+  modal.classList.add('active');
+  if(overlay != null) {
+    overlay.classList.add('active');
+  } else if (overlay1 != null) {
+    overlay1.classList.add('active');
+  } else {
+    return
+  }
+
 }
 
 function closeModal(modal) {
   if(modal == null) return;
-  modal.classList.remove('add');
+  modal.classList.remove('active');
+  if(overlay != null) {
+    overlay.classList.remove('active');
+  } else if (overlay1 != null) {
+    overlay1.classList.remove('active');
+  } else {
+    return
+  }
 }
