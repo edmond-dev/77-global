501e79ed631847b1d2d69bfca6143ace835f028c
Edmond Mumwensi <edmondprojects71@gamil.com>
2020-10-22 23:58:41 -0600

    disconnected db

--- dbconfig/connections.js
+++ dbconfig/connections.js
@@ -1,21 +1,21 @@
-'use strict';
-
-const mysql = require('mysql');
-//------------------------------
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.MYSQL_DB,
-  password: process.env.DB_PASS,
-  multistatement: true
-});
-db.connect((err)=>{
-  if(!err){
-    console.log('connected to mysql.');
-  }else{
-    console.log(err);
-  };
-});
-
-
-module.exports = db;
+// 'use strict';
+//
+// const mysql = require('mysql');
+// //------------------------------
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.MYSQL_DB,
+//   password: process.env.DB_PASS,
+//   multistatement: true
+// });
+// db.connect((err)=>{
+//   if(!err){
+//     console.log('connected to mysql.');
+//   }else{
+//     console.log(err);
+//   };
+// });
+//
+//
+// module.exports = db;
